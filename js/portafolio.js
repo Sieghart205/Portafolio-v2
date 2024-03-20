1c5c548b8677e04adf93362ac3bb156c10652097
Sieghart <sieghart2005@gmail.com>
2024-03-20 17:38:50 -0500

    first commit

---
+++ js/portafolio.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    reset:true,
+    distance:"60px",
+    duration:"2500",
+})
+
+ScrollReveal().reveal(".main-title", {delay:200, origin:"top"})
+ScrollReveal().reveal(".content, .slider", {delay:500,origin:"bottom"})
+ScrollReveal().reveal(".block, .info",{delay:700,origin:"right"})
