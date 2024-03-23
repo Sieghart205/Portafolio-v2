f9f576abe23e1a9725cc0e37e34309f066acf399
Sieghart <sieghart2005@gmail.com>
2024-03-23 12:10:06 -0500

    añadiendo algunos detalles al acerca de

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const Facebook = document.getElementById("facebook");
+const x = document.getElementById("x");
+const pinterest = document.getElementById("pinterest");
+
+Facebook.addEventListener("click",()=>{
+    window.open("https://www.facebook.com/profile.php?id=100075133152053");
+})
+
+x.addEventListener("click",()=>{
+    window.open("https://twitter.com/JeissonBenitez3");
+})
+
+pinterest.addEventListener("click",()=>{
+    window.open("https://co.pinterest.com/benitezortegajeisson/_created/");
+})
