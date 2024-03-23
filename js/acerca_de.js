475eb68ecf8aaec2b11f5f1d4977fd595c9da11e
Sieghart <sieghart2005@gmail.com>
2024-03-23 00:20:53 -0500

    se realizaron algunas modificaciones a la vista de acerca_de y contacto

--- js/acerca_de.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", () => {
-    console.log("scrolling");
-    const barraLateral = document.getElementById("barra-lateral");
-    barraLateral.classList.toggle("scroll", window.scrollY > 0);
-});
