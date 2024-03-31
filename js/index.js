8c548b7b1271be98f2f7ed7f5b680cd2eeabae02
Sieghart <sieghart2005@gmail.com>
2024-03-31 15:55:37 -0500

    formato de codigo y espaciado en la vista de acerca de

--- js/index.js
+++ js/index.js
@@ -2,14 +2,14 @@ const Facebook = document.getElementById("facebook");
 const x = document.getElementById("x");
 const pinterest = document.getElementById("pinterest");
 
-Facebook.addEventListener("click",()=>{
-    window.open("https://www.facebook.com/profile.php?id=100075133152053");
-})
+Facebook.addEventListener("click", () => {
+  window.open("https://www.facebook.com/profile.php?id=100075133152053");
+});
 
-x.addEventListener("click",()=>{
-    window.open("https://twitter.com/JeissonBenitez3");
-})
+x.addEventListener("click", () => {
+  window.open("https://twitter.com/JeissonBenitez3");
+});
 
-pinterest.addEventListener("click",()=>{
-    window.open("https://co.pinterest.com/benitezortegajeisson/_created/");
-})
+pinterest.addEventListener("click", () => {
+  window.open("https://co.pinterest.com/benitezortegajeisson/_created/");
+});
